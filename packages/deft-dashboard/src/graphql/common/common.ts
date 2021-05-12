5e7e73b4f8d158c2fdb0d3a729301f4c4a902474
egoarka <egoarka@gmail.com>
2021-05-12 22:25:26 +0300

    feat: provide full info about referrals and referrer

--- packages/deft-dashboard/src/graphql/common/common.ts
+++ packages/deft-dashboard/src/graphql/common/common.ts
@@ -1,19 +1,39 @@
 import gql from "graphql-tag";
 
 export const someQuery = gql`
+  query Multiplier {
+    multiplier(id: 1) {
+      id
+      value
+    }
+  }
+
   query UserReferrals2lvl($address: ID!) {
     user(id: $address) {
       id
-      reward
+      createdAt
+      referralsCount
       referrer {
         id
-      }
+        referralsCount
+      } # lvl 1
       referrals(first: 1000) {
         id
-        reward
+        createdAt
+        referralsCount
+        referrer {
+          id
+          referralsCount
+        }
+        # lvl 2
         referrals(first: 1000) {
           id
-          reward
+          createdAt
+          referralsCount
+          referrer {
+            id
+            referralsCount
+          }
         }
       }
     }
