9ebfd9421d80a5650c24ac8e9330359a4e4f40b9
egoarka <egoarka@gmail.com>
2021-07-05 18:05:25 +0300

    feat: deploy and update

--- packages/deft-bot-detector/update.sh
+++
@@ -1,9 +0,0 @@
-export $(grep -v '^#' .env | xargs)
-
-eval `ssh-agent -s`
-ssh-add ~/.ssh/sms_rsa
-
-git pull
-
-docker build . --tag=bot-detector-server
-docker stack deploy  -c ./stack.prod.yml bot-detector-stack
