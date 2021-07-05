a9cb31a70c6a3300ae00bc0ae5e4342c7b03065c
egoarka <egoarka@gmail.com>
2021-07-05 18:37:45 +0300

    chore: resolve image never

--- packages/deft-bot-detector/deploy-and-update.sh
+++ packages/deft-bot-detector/deploy-and-update.sh
@@ -4,5 +4,5 @@ ssh-add ~/.ssh/sms_rsa
 git pull
 
 docker build . --tag=bot-detector-server
-docker stack deploy  -c ./stack.prod.yml bot-detector-stack
+docker stack deploy --resolve-image=never -c ./stack.prod.yml bot-detector-stack
 docker service update --force bot-detector-stack_deft-detector
