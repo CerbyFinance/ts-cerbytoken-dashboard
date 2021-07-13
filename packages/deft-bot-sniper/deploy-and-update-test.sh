102b0bbe34082973927a7a194eb19e37b19e40bd
egoarka <egoarka@gmail.com>
2021-07-13 20:34:33 +0300

    feat: add deployment for test stack

---
+++ packages/deft-bot-sniper/deploy-and-update-test.sh
@@ -0,0 +1,8 @@
+eval `ssh-agent -s`
+ssh-add ~/.ssh/sms_rsa
+
+git pull
+
+docker build . --tag=bot-sniper-server
+docker stack deploy --resolve-image=never -c ./stack.prod.yml bot-sniper-test-stack
+docker service update --force bot-sniper-test-stack_deft-sniper
