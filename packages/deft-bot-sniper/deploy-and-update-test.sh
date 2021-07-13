97f3871d3aee2f4f92507e39e1c4c5b446adddde
egoarka <egoarka@gmail.com>
2021-07-13 20:35:49 +0300

    chore: set test stack

--- packages/deft-bot-sniper/deploy-and-update-test.sh
+++ packages/deft-bot-sniper/deploy-and-update-test.sh
@@ -4,5 +4,5 @@ ssh-add ~/.ssh/sms_rsa
 git pull
 
 docker build . --tag=bot-sniper-server
-docker stack deploy --resolve-image=never -c ./stack.prod.yml bot-sniper-test-stack
+docker stack deploy --resolve-image=never -c ./stack.test.yml bot-sniper-test-stack
 docker service update --force bot-sniper-test-stack_deft-sniper
