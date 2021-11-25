3484d072e72c4f00cc058bb124ecdf400e2bc123
Egor <egor@gmail.com>
2021-11-25 19:49:17 +0300

    feat: add fantom & avalanche

--- packages/deft-staking-subgraph/graph-remote.sh
+++ packages/deft-staking-subgraph/graph-remote.sh
@@ -4,4 +4,4 @@
 # create
 # remove
 
-graph $1 --node http://nodes2.valar-solutions.com:8020/ --ipfs http://nodes2.valar-solutions.com:5001/ deft/staking-$2 ./subgraph.$2.yaml
+graph $1 --node http://nodes2.valar-solutions.com:8020/ --ipfs http://nodes2.valar-solutions.com:5001/ cerby/staking-$2 ./subgraph.$2.yaml
