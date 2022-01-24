157b4e563006d1b34df2ba4d122645dd56282a7f
Egor <egor@gmail.com>
2022-01-25 01:29:42 +0300

    feat: remove api

--- packages/deft-bridge-validator/src/main.ts
+++ packages/deft-bridge-validator/src/main.ts
@@ -1,25 +1,3 @@
-import { NestFactory } from "@nestjs/core";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { AppModule } from "./app.module";
 import { triggerRunJobs } from "./features/bridge";
 
-async function bootstrap() {
-  triggerRunJobs();
-
-  const app = await NestFactory.create(AppModule);
-
-  let options = new DocumentBuilder()
-    .setTitle("api")
-    .setDescription("api description")
-    .setVersion("1.0");
-
-  const builtOptions = options.build();
-
-  const document = SwaggerModule.createDocument(app, builtOptions);
-  SwaggerModule.setup("api/docs", app, document);
-
-  app.enableCors();
-
-  await app.listen(3001);
-}
-bootstrap();
+triggerRunJobs();
